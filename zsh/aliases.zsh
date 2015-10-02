de2b1e448dd10a8fec3909c486213dc2f90a9ef4
DFischer <me@danielfischer.com>
2015-10-01 22:03:18 -0700

    shortcuts

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
-alias meteorgo="DDP_DEFAULT_CONNECTION_URL=http://localhost:3030 meteor --settings=settings.json"
+alias tuul="DDP_DEFAULT_CONNECTION_URL=http://localhost:3030 meteor --settings=settings.json"
+alias tuuldebug="DEBUG=true DDP_DEFAULT_CONNECTION_URL=http://localhost:3030 meteor --settings=settings.json"
