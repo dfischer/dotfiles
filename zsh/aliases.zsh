d4bd0d271b0a275ac49418238322f05e133e783b
DFischer <me@danielfischer.com>
2017-10-11 00:35:26 -0700

    Remove old stuff. Hyper terminal default.

--- zsh/aliases.zsh
+++
@@ -1,2 +0,0 @@
-alias tuul="DDP_DEFAULT_CONNECTION_URL=http://localhost:3030 meteor --settings=settings.json"
-alias tuuldebug="DEBUG=true DDP_DEFAULT_CONNECTION_URL=http://localhost:3030 meteor --settings=settings.json"
--- zsh/defaults.zsh
+++
@@ -1,5 +0,0 @@
-source $HOME/.dotfiles/zsh/aliases.zsh
-source $HOME/.dotfiles/zsh/git-flow-completion.zsh
-
-export VISUAL=vim
-export EDIT="$VISUAL"
